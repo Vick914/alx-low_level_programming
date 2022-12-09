3c3c107626521ea9050d0a7e32df9088ccf4e949
vick914 <oseyomonvictor123@gmail.com>
2022-12-08 19:54:20 -0800

    Vick coder

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
