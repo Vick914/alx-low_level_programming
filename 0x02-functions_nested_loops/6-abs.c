897d8d56a61ba67d8e52828ea9fdbb89bc327735
vick914 <oseyomonvictor123@gmail.com>
2022-12-13 19:02:32 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+/**
+ *_abs - Check Main
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
