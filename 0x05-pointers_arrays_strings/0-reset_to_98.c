fd70e9f0fe3dd9dfaef4023d080b6aaea27b8d03
vick914 <oseyomonvictor123@gmail.com>
2022-12-19 15:43:38 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Vick_Code
+*  Return: 0 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
