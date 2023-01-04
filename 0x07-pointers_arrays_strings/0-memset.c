c0531c014510fb339cb33add1a58fd22b7279b93
vick914 <oseyomonvictor123@gmail.com>
2023-01-03 18:58:59 -0800

    points and array

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,26 +1,41 @@
 #include "main.h"
 
+
+
 /**
- * _memset - function fill the first @n bytes of the memory area pointed
- * to by @s with the constant byte @b
  *
- * @n: bytes of the memory area pointed to by @s
+ *  * *_memset - fills memory with a constant byte
  *
- * @s: with the constant byte @b
+ *   * @s: memory area to be filled
  *
- * @b: memory area pointer
+ *    * @b: char to copy
  *
- * Return: a pointer to the memory area @s
- */
+ *     * @n: number of times to copy b
+ *
+ *      *
+ *
+ *       * Return: pointer to the memory area s
+ *
+ *        */
+
 char *_memset(char *s, char b, unsigned int n)
+
 {
-	unsigned int i = 0;
 
-	while (i < n)
-	{
-	s[i] = b;
-	i++;
-	}
+		unsigned int i;
 
-	return (s);
+
+
+			for (i = 0; i < n; i++)
+
+					{
+
+								s[i] = b;
+
+									}
+
+
+
+				return (s);
+
 }
